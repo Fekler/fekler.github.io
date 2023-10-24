7859026757615d750fb55278c1a70574000b3027
Felipe Queiroz <fekler.jobs@gmail.com>
2023-10-24 16:09:53 -0300

    Muitas Implementações

---
+++ Scripts/DivContact.js
@@ -0,0 +1,52 @@
+var imagesContact = document.querySelectorAll('.divContactIcon span');
+var prevContactButton = document.getElementById('prev-contact-button');
+var nextContactButton = document.getElementById('next-contact-button');
+var currentContactIndex = 0;
+
+function showImagesContact() {
+    var windowWidth = window.innerWidth;
+    var numImagesToShow = 7;
+
+    if (windowWidth < 768) {
+        numImagesToShow = 2;
+    } else if (windowWidth < 1024) {
+        numImagesToShow = 5;
+    }
+
+    imagesContact.forEach(function (image, index) {
+        if (index >= currentContactIndex && index < currentContactIndex + numImagesToShow) {
+            image.style.display = 'block';
+        } else {
+            image.style.display = 'none';
+        }
+    });
+
+    prevContactButton.style.display = currentContactIndex > 0 ? 'block' : 'none';
+    nextContactButton.style.display = currentContactIndex + numImagesToShow < imagesContact.length ? 'block' : 'none';
+}
+
+prevContactButton.addEventListener('click', function () {
+    if (currentContactIndex > 0) {
+        currentContactIndex -= 1;
+        showImagesContact();
+    }
+});
+
+nextContactButton.addEventListener('click', function () {
+    var windowWidth = window.innerWidth;
+    var numImagesToShow = 7;
+
+    if (windowWidth < 768) {
+        numImagesToShow = 2;
+    } else if (windowWidth < 1024) {
+        numImagesToShow = 5;
+    }
+
+    if (currentContactIndex + numImagesToShow < imagesContact.length) {
+        currentContactIndex += 1;
+        showImagesContact();
+    }
+});
+
+showImagesContact();
+window.addEventListener('resize', showImagesContact);
--- Scripts/LocalizationStrings.js
+++ Scripts/LocalizationStrings.js
@@ -21,28 +21,40 @@ class LocalizationStrings {
         en: "Hello, I am Felipe Queiroz",
         pt: "Olá, eu sou o Felipe Queiroz"
     };
-    
-    
+
+
     static PERFIL = {
         en: "",
         pt: ""
     };
-    
-    
+
+
     static SKILLS_TITLE = {
-        en:"Skills: ",
-        pt:"Habilidades"
+        en: "Skills: ",
+        pt: "Habilidades"
 
     };
-    
+    static CONTACT_TITLE = {
+        en: "Let's connect ",
+        pt: "Vamos nos conectar"
+
+    };
+    static CONTACT_P = {
+        en: "Below is my social media and contact information:",
+        pt: "Abaixo segue minhas redes sociais e informações de contato:"
+
+    };
+
     static RIGHTER = {
-        en:"© 2023 Felipe Queiroz",
-        pt:"© 2023 Felipe Queiroz"
+        en: "© 2023 Felipe Queiroz",
+        pt: "© 2023 Felipe Queiroz"
 
     };
 }
 
 function changeLanguage(selectedLanguage) {
+    document.documentElement.lang = LocalizationStrings.LANG[selectedLanguage];
+
     var aboutLink = document.getElementById("about-link");
 
     var portfolioLink = document.getElementById("portfolio-link");
@@ -51,21 +63,19 @@ function changeLanguage(selectedLanguage) {
     var greetingLink = document.getElementById("greeting-link");
 
     var perfilLink = document.getElementById("perfil-link");
+
+    var contactTitleLink = document.getElementById("contact-title-link");
+    var contactContentLink = document.getElementById("contact-content-link");
     var righterLink = document.getElementById("righter-link");
 
 
-    document.documentElement.lang = LocalizationStrings.LANG[selectedLanguage];
-
     aboutLink.textContent = LocalizationStrings.NAV_ABOUT[selectedLanguage];
-
     portfolioLink.textContent = LocalizationStrings.NAV_PORTFOLIO[selectedLanguage];
-
     contactLink.textContent = LocalizationStrings.NAV_CONTACT[selectedLanguage];
-
     greetingLink.textContent = LocalizationStrings.GREETING[selectedLanguage];
-
+    contactTitleLink.textContent = LocalizationStrings.CONTACT_TITLE[selectedLanguage];
+    contactContentLink.textContent = LocalizationStrings.CONTACT_P[selectedLanguage];
     perfilLink.textContent = LocalizationStrings.PERFIL[selectedLanguage];
-
     righterLink.textContent = LocalizationStrings.RIGHTER[selectedLanguage];
 
 }
