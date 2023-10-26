9f22f7c1c2803069617b3748addbdc67cc297a91
Felipe Queiroz <fekler.jobs@gmail.com>
2023-10-25 21:55:02 -0300

    save

--- Scripts/LocalizationStrings.js
+++ Scripts/LocalizationStrings.js
@@ -104,3 +104,4 @@ document.addEventListener("DOMContentLoaded", function () {
     // Coloque seu código JavaScript aqui para garantir que a página tenha sido carregada completamente.
     changeLanguage("pt"); // Defina o idioma padrão aqui, se necessário.
 });
+
